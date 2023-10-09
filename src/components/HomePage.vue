4ba1919f46b010431d4974f0ddfa4a717b43ac8f
Sean Kelliher <sbkelliher@icloud.com>
2023-10-09 00:44:50 -0400

    Added style elements like fieldset to form. Modified 'section' where results appear.

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { senators } from "../data/us-senators-with-stats.js";
+import HeaderPart from "./HeaderPart.vue";
 import { ref, computed, onMounted } from "vue";
 
 // Variables for categories.
@@ -95,123 +96,133 @@ function processInputs() {
     });
     checkCommons();
 }
-
-/*function checkCommons() {
-    const commonsLength = commons.value.length;
-    if (commonsLength === 0) {
-        const noResults = document.getElementById("no-results");
-        noResults.style.color = "red";
-    }
-}*/
 </script>
 
+<!--add tabbing index to all applicable elements -->
 <template>
-    <form @submit.prevent="processInputs()">
-        <p>All fields are optional</p>
-        <label>Name</label>
-        <input
-            :tabindex="0"
-            v-model="searchTerm"
-            placeholder="Ex., Susan Collins"
-        />
+    <div id="container">
 
-        <div>Party</div>
-        <input type="radio" id="all-parties" value="All Parties" v-model="party" />
-        <label for="all-parties">All</label>
-        <input type="radio" id="democrat" value="Democrat" v-model="party" />
-        <label for="democrat">Democrat</label>
-        <input type="radio" id="republican" value="Republican" v-model="party" />
-        <label for="republican">Republican</label>
-        <input type="radio" id="independent" value="Independent" v-model="party" />
-        <label for="independent">Independent</label>
+        <HeaderPart headline="A directory for the United States Senate" />
 
-        <div>Gender</div>
-        <input type="radio" id="all-genders" value="All Genders" v-model="gender" />
-        <label for="all-genders">All</label>
-        <input type="radio" id="male" value="male" v-model="gender" />
-        <label for="male">male</label>
-        <input type="radio" id="female" value="female" v-model="gender" />
-        <label for="female">female</label>
+        <main>
+            <form @submit.prevent="processInputs()">
+                <fieldset>
+                    <legend>Name</legend>
+                    <input
+                        type="text"
+                        id="search-term"
+                        :tabindex="0"
+                        v-model="searchTerm"
+                        placeholder="Ex., Susan Collins"
+                    />
+                </fieldset>
 
-        <div>Up for re-election</div>
-        <input type="radio" id="all-elections" value="All Elections" v-model="reelection" />
-        <label for="all-elections">All</label>
-        <input type="radio" id="class1" value="class1" v-model="reelection" />
-        <label for="class1">November 2024</label>
-        <input type="radio" id="class2" value="class2" v-model="reelection" />
-        <label for="class2">November 2026</label>
-        <input type="radio" id="class3" value="class3" v-model="reelection" />
-        <label for="class3">November 2028</label>
+                <fieldset>
+                    <legend>Party</legend>
+                    <input type="radio" id="all-parties" value="All Parties" v-model="party" />
+                    <label for="all-parties">All</label><br />
+                    <input type="radio" id="democrat" value="Democrat" v-model="party" />
+                    <label for="democrat">Democrat</label><br />
+                    <input type="radio" id="republican" value="Republican" v-model="party" />
+                    <label for="republican">Republican</label><br />
+                    <input type="radio" id="independent" value="Independent" v-model="party" />
+                    <label for="independent">Independent</label>
+                </fieldset>
 
-        <div>State</div>
-        <select v-model="homeState">
-            <!--<option disabled value="">Please select one</option>-->
-            <option>All States</option>
-            <option>Alabama</option>
-            <option>Alaska</option>
-            <option>Arizona</option>
-            <option>Arkansas</option>
-            <option>California</option>
-            <option>Colorado</option>
-            <option>Connecticut</option>
-            <option>Delaware</option>
-            <option>Florida</option>
-            <option>Georgia</option>
-            <option>Hawaii</option>
-            <option>Idaho</option>
-            <option>Illinois</option>
-            <option>Indiana</option>
-            <option>Iowa</option>
-            <option>Kansas</option>
-            <option>Kentucky</option>
-            <option>Louisiana</option>
-            <option>Maine</option>
-            <option>Maryland</option>
-            <option>Massachusetts</option>
-            <option>Michigan</option>
-            <option>Minnesota</option>
-            <option>Mississippi</option>
-            <option>Missouri</option>
-            <option>Montana</option>
-            <option>Nebraska</option>
-            <option>Nevada</option>
-            <option>New Hampshire</option>
-            <option>New Jersey</option>
-            <option>New Mexico</option>
-            <option>New York</option>
-            <option>North Carolina</option>
-            <option>North Dakota</option>
-            <option>Ohio</option>
-            <option>Oklahoma</option>
-            <option>Oregon</option>
-            <option>Pennsylvania</option>
-            <option>Rhode Island</option>
-            <option>South Carolina</option>
-            <option>South Dakota</option>
-            <option>Tennessee</option>
-            <option>Texas</option>
-            <option>Utah</option>
-            <option>Vermont</option>
-            <option>Virginia</option>
-            <option>Washington</option>
-            <option>West Virginia</option>
-            <option>Wisconsin</option>
-            <option>Wyoming</option>
-        </select>
+                <fieldset>
+                    <legend>Gender</legend>
+                    <input type="radio" id="all-genders" value="All Genders" v-model="gender" />
+                    <label for="all-genders">All</label><br />
+                    <input type="radio" id="male" value="male" v-model="gender" />
+                    <label for="male">Male</label><br />
+                    <input type="radio" id="female" value="female" v-model="gender" />
+                    <label for="female">Female</label>
+                </fieldset>
 
-        <button type="submit">Submit</button>
+                <fieldset>
+                    <legend>Up for re-election</legend>
+                    <input type="radio" id="all-elections" value="All Elections" v-model="reelection" />
+                    <label for="all-elections">All</label><br />
+                    <input type="radio" id="class1" value="class1" v-model="reelection" />
+                    <label for="class1">November 2024</label><br />
+                    <input type="radio" id="class2" value="class2" v-model="reelection" />
+                    <label for="class2">November 2026</label><br />
+                    <input type="radio" id="class3" value="class3" v-model="reelection" />
+                    <label for="class3">November 2028</label>
+                </fieldset>
 
-    </form>
+                <fieldset>
+                    <legend>State</legend>
+                    <select v-model="homeState">
+                        <!--<option disabled value="">Please select one</option>-->
+                        <option>All States</option>
+                        <option>Alabama</option>
+                        <option>Alaska</option>
+                        <option>Arizona</option>
+                        <option>Arkansas</option>
+                        <option>California</option>
+                        <option>Colorado</option>
+                        <option>Connecticut</option>
+                        <option>Delaware</option>
+                        <option>Florida</option>
+                        <option>Georgia</option>
+                        <option>Hawaii</option>
+                        <option>Idaho</option>
+                        <option>Illinois</option>
+                        <option>Indiana</option>
+                        <option>Iowa</option>
+                        <option>Kansas</option>
+                        <option>Kentucky</option>
+                        <option>Louisiana</option>
+                        <option>Maine</option>
+                        <option>Maryland</option>
+                        <option>Massachusetts</option>
+                        <option>Michigan</option>
+                        <option>Minnesota</option>
+                        <option>Mississippi</option>
+                        <option>Missouri</option>
+                        <option>Montana</option>
+                        <option>Nebraska</option>
+                        <option>Nevada</option>
+                        <option>New Hampshire</option>
+                        <option>New Jersey</option>
+                        <option>New Mexico</option>
+                        <option>New York</option>
+                        <option>North Carolina</option>
+                        <option>North Dakota</option>
+                        <option>Ohio</option>
+                        <option>Oklahoma</option>
+                        <option>Oregon</option>
+                        <option>Pennsylvania</option>
+                        <option>Rhode Island</option>
+                        <option>South Carolina</option>
+                        <option>South Dakota</option>
+                        <option>Tennessee</option>
+                        <option>Texas</option>
+                        <option>Utah</option>
+                        <option>Vermont</option>
+                        <option>Virginia</option>
+                        <option>Washington</option>
+                        <option>West Virginia</option>
+                        <option>Wisconsin</option>
+                        <option>Wyoming</option>
+                    </select>
+                </fieldset>
+                <button type="submit">Submit</button>
 
-    <section>
-        <p id="no-results">No matching results.</p>
-    </section>
-
-    <section v-for="senator in senators" :key="senator.bioguideId">
-        <div v-if="commons.includes(senator.bioguideId)">
-            <p>{{ senator.firstName }} {{ senator.lastName }}</p>
-        </div>
-    </section>
+                <!-- ADD A CLEAR ALL BUTTON TO THE FORM -->
+            </form>
+            <section>
+                <p id="no-results">No matching results.</p>
+                <template v-for="senator in senators" :key="senator.bioguideId">
+                    <div v-if="commons.includes(senator.bioguideId)" class="card">
+                        <p>{{ senator.firstName }} {{ senator.lastName }}</p>
+                        <p>{{ senator.state }} - {{ senator.party }}</p>
+                    </div>
+                </template>
+            </section>
+        </main>
+    </div>
 </template>
 
 <style scoped>
