479e07c2eda2ddbf54693f7f56fb5ea1bdec01db
Sean Kelliher <sbkelliher@icloud.com>
2023-10-09 00:42:13 -0400

    Added a 'header' component.

---
+++ src/components/HeaderPart.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    headline: {
+        type: String,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <header>
+        <h1>{{ headline }}</h1>
+    </header>
+</template>
